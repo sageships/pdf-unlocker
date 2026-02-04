e1c8b1c6abd51a5b27fc31235ef196904a86b9b0
sageships <sage.ships@gmail.com>
2026-02-04 20:54:39 +0530

    Fix TypeScript blob type error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
       const pdfBytes = await unlockedPdf.save();
       
       // Download
-      const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+      const blob = new Blob([pdfBytes.buffer], { type: 'application/pdf' });
       const url = URL.createObjectURL(blob);
       const a = document.createElement('a');
       a.href = url;
